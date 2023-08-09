1f97b7e3c00647736bf737e75d5ace5ee85f83c8
anthle <su810152974@gmail.com>
2023-08-10 01:24:42 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{
+  modelValue?: boolean,
+}>()
+
+</script>
+
+<template>
+  <div>
+    <NuxtLink to="/ranks"><h1>hello world</h1></NuxtLink>
+  </div>
+</template>
---
+++ pages/ranks.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{
+  modelValue?: boolean,
+}>()
+
+</script>
+
+<template>
+  <div>
+    <NuxtLink to="/"><h1>ranks</h1></NuxtLink>
+  </div>
+</template>
